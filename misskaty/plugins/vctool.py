e4a64c0a031370fcb37830dc410c0d9821509300
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-10-20 15:53:01 +0530

    Create vctool.py

---
+++ misskaty/plugins/vctool.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from misskaty import app
+from config import OWNER_ID
+
+
+
+@app.on_message(filters.video_chat_started)
+async def brah(_, msg):
+       await msg.reply("Voice chat Started 🎙️")
+
+
+@app.on_message(filters.video_chat_ended)
+async def brah2(_, msg):
+       await msg.reply("**Voice chat Ended 🔇**")
+
+
+@app.on_message(filters.video_chat_members_invited)
+async def brah3(app :app, message:Message):
+           text = f"{message.from_user.mention} ɪɴᴠɪᴛᴇᴅ "
+           x = 0
+           for user in message.video_chat_members_invited.users:
+             try:
+               text += f"[{user.first_name}](tg://user?id={user.id}) "
+               x += 1
+             except Exception:
+               pass
+           try:
+             await message.reply(f"{text} 💞")
+           except:
+             pass
