7478242ad01b2f41ac43fc0657e7b46f35933c2b
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-11-01 21:28:46 +0530

    Update ai_draw.py

--- misskaty/plugins/ai_draw.py
+++ misskaty/plugins/ai_draw.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, filters
 import requests
 from misskaty import app
+import time
+import os
 
 # Generate a detailed prompt for image creation
 def generate_long_query(query):
@@ -38,3 +40,55 @@ async def draw_image(client, message):
             await wait_message.edit_text("Error: Unable to generate image at this time. Please try later. 🚫")
     except Exception as e:
         await wait_message.edit_text(f"An error occurred: {e} ⚠️")
+
+ 
+
+# Command handler for /gen
+@app.on_message(filters.command(['imagine','generate']))
+async def generate_image(client, message):
+    # Get the prompt from the command
+    prompt = ' '.join(message.command[1:])
+
+    # Send a message to inform the user to wait
+    wait_message = await message.reply_text("Please wait while I generate the image...")
+    StartTime = time.time()
+
+
+    # API endpoint URL
+    url = 'https://ai-api.magicstudio.com/api/ai-art-generator'
+
+    # Form data for the request
+    form_data = {
+        'prompt': prompt,
+        'output_format': 'bytes',
+        'request_timestamp': str(int(time.time())),
+        'user_is_subscribed': 'false',
+    }
+
+    # Send a POST request to the API
+    response = requests.post(url, data=form_data)
+
+    if response.status_code == 200:
+        try:
+            if response.content:
+                destination_dir = ''
+                destination_path = os.path.join(destination_dir, 'generated_image.jpg')
+
+                # Save the image to the destination path
+                with open(destination_path, 'wb') as f:
+                    f.write(response.content)
+
+                # Delete the wait message
+                await wait_message.delete()
+
+                # Send the generated image
+                await message.reply_photo(destination_path, caption=f"Here's the generated image!\nTime Taken: {time.time() - StartTime}")
+
+                # Delete the generated image after sending
+                os.remove(destination_path)
+            else:
+                await wait_message.edit_text("Failed to generate the image.")
+        except Exception as e:
+            await wait_message.edit_text("Error: {}".format(e))
+    else:
+        await wait_message.edit_text("Error: {}".format(response.status_code))
