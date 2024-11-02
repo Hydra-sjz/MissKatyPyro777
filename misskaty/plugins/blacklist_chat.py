8ea91a30698a18d88ad57404310250a5968339f6
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-11-02 21:28:52 +0530

    Update blacklist_chat.py

--- misskaty/plugins/blacklist_chat.py
+++ misskaty/plugins/blacklist_chat.py
@@ -12,7 +12,7 @@ from misskaty.ultis_ex.database.dbfunctions import (
 )
 
 
-@app.on_message(filters.command("blacklist_chat") & SUDOERS)
+@app.on_message(filters.command("blacklist_chat") & filters.user(SUDOERS))
 @capture_err
 async def blacklist_chat_func(_, message: Message):
     if len(message.command) != 2:
@@ -30,7 +30,7 @@ async def blacklist_chat_func(_, message: Message):
     await message.reply_text("Something wrong happened, check logs.")
 
 
-@app.on_message(filters.command("whitelist_chat") & SUDOERS)
+@app.on_message(filters.command("whitelist_chat") & filters.user(SUDOERS))
 @capture_err
 async def whitelist_chat_func(_, message: Message):
     if len(message.command) != 2:
@@ -48,7 +48,7 @@ async def whitelist_chat_func(_, message: Message):
     await message.reply_text("Something wrong happened, check logs.")
 
 
-@app.on_message(filters.command("blacklisted_chats") & SUDOERS)
+@app.on_message(filters.command("blacklisted_chats") & filters.user(SUDOERS))
 @capture_err
 async def blacklisted_chats_func(_, message: Message):
     text = ""
