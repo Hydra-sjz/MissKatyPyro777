53b3ca468625836d933b4b18e5cdd44095725110
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-11-03 21:26:51 +0530

    Update db.py

--- anilist/db.py
+++ anilist/db.py
@@ -1,21 +1,33 @@
  # below code is taken from USERGE-X repo
 # all credits to the respective author (dunno who wrote it will find later
-# n update)
-
-
-__all__ = ["get_collection"]
+# n update
 
 #from motor.core import AgnosticClient, AgnosticCollection, AgnosticDatabase
 #from motor.motor_asyncio import AsyncIOMotorClient
 
-from async_pymongo import AsyncClient
-#from pymongo import MongoClient
+#from async_pymongo import AsyncClient
+#from misskaty.vars import DATABASE_URI as MONGO_DB_URL
+
+#MGCLIENT = AsyncClient(MONGO_DB_URL)
+#DATABASE = MGCLIENT["Emilia"]
+
+
+#def get_collection(name: str):
+#    """Create or Get Collection from your database"""
+#    return DATABASE[name]
+
+__all__ = ["get_collection"]
+
+from motor.core import AgnosticClient, AgnosticCollection, AgnosticDatabase
+from motor.motor_asyncio import AsyncIOMotorClient
+
 from misskaty.vars import DATABASE_URI as MONGO_DB_URL
 
-MGCLIENT = AsyncClient(MONGO_DB_URL)
-DATABASE = MGCLIENT["Emilia"]
+_MGCLIENT: AgnosticClient = AsyncIOMotorClient(MONGO_DB_URL)
+
+_DATABASE: AgnosticDatabase = _MGCLIENT["Emilia"]
 
 
-def get_collection(name: str):
+def get_collection(name: str) -> AgnosticCollection:
     """Create or Get Collection from your database"""
-    return DATABASE[name]
+    return _DATABASE[name]
