140e958ffda1ed78a89cf85e90c978252b39e6ae
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-11-03 12:40:17 +0530

    Update dbfunctions.py

--- misskaty/ultis_ex/database/dbfunctions.py
+++ misskaty/ultis_ex/database/dbfunctions.py
@@ -49,19 +49,23 @@ async def get_served_chats() -> list:
         chats_list.append(chat)
     return chats_list
     
-
+async def is_served_chat(chat_id: int) -> bool:
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
 async def add_served_chat(chat_id: int):
     is_served = await is_served_chat(chat_id)
     if is_served:
         return
     return await chatsdb.insert_one({"chat_id": chat_id})
-    
+
+
 async def is_served_user(user_id: int) -> bool:
     user = await usersdb.find_one({"user_id": user_id})
     if not user:
         return False
     return True
-    
 async def add_served_user(user_id: int):
     is_served = await is_served_user(user_id)
     if is_served:
