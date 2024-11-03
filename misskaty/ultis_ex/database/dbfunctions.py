fe8c3b8a8cc3099f65190758b83df7a221f39875
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-11-03 11:57:33 +0530

    Update dbfunctions.py

--- misskaty/ultis_ex/database/dbfunctions.py
+++ misskaty/ultis_ex/database/dbfunctions.py
@@ -39,42 +39,16 @@ chatsdb = db.chats
 antiservicedb = db.antiservice
 blacklist_chatdb = db.blacklistChat
 rssdb = db.rss
+
+
+
+
 #=========
-async def is_served_chat(chat_id: int) -> bool:
-    chat = await chatsdb.find_one({"chat_id": chat_id})
-    if not chat:
-        return False
-    return True
-
-async def get_served_chats() -> list:
-    chats_list = []
-    async for chat in chatsdb.find({"chat_id": {"$lt": 0}}):
-        chats_list.append(chat)
-    return chats_list
-
 async def add_served_chat(chat_id: int):
     is_served = await is_served_chat(chat_id)
     if is_served:
         return
     return await chatsdb.insert_one({"chat_id": chat_id})
-
-async def remove_served_chat(chat_id: int):
-    is_served = await is_served_chat(chat_id)
-    if not is_served:
-        return
-    return await chatsdb.delete_one({"chat_id": chat_id})
-
-async def is_served_user(user_id: int) -> bool:
-    user = await usersdb.find_one({"user_id": user_id})
-    if not user:
-        return False
-    return True
-
-async def get_served_users() -> list:
-    users_list = []
-    async for user in usersdb.find({"user_id": {"$gt": 0}}):
-        users_list.append(user)
-    return users_list
 
 async def add_served_user(user_id: int):
     is_served = await is_served_user(user_id)
