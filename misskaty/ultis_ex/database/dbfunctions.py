97bcb858d6ff9f9d9f8fb0d718d783562699bd65
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-11-03 13:16:06 +0530

    Update dbfunctions.py

--- misskaty/ultis_ex/database/dbfunctions.py
+++ misskaty/ultis_ex/database/dbfunctions.py
@@ -33,7 +33,7 @@ from misskaty.ultis_ex.database.willam_bd import db
 # AND NEEDS TO BE REWRITTEN, BUT I WON'T, AS IT WILL TAKE
 # TOO MUCH TIME AND WILL BE BAD FOR ALREADY STORED DATA
 
-
+flood_toggle_db = db.flood_toggle
 usersdb = db.users
 chatsdb = db.chats
 blacklist_chatdb = db.blacklistChat
@@ -41,7 +41,24 @@ rssdb = db.rss
 
 
 
+async def is_flood_on(chat_id: int) -> bool:
+    chat = await flood_toggle_db.find_one({"chat_id": chat_id})
+    if not chat:
+        return True
+    return False
 
+async def flood_on(chat_id: int):
+    is_flood = await is_flood_on(chat_id)
+    if is_flood:
+        return
+    return await flood_toggle_db.delete_one({"chat_id": chat_id})
+
+async def flood_off(chat_id: int):
+    is_flood = await is_flood_on(chat_id)
+    if not is_flood:
+        return
+    return await flood_toggle_db.insert_one({"chat_id": chat_id})
+    
 #=========
 async def get_served_chats() -> list:
     chats_list = []
