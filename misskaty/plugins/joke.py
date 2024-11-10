64eb166c5642ff1b438f6f183fd8234772e465f2
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-11-10 19:08:07 +0530

    Create joke.py

---
+++ misskaty/plugins/joke.py
@@ -0,0 +1,25 @@
+from pyrogram import Client
+from requests import get
+
+from misskaty import app
+from misskaty.ultis_ex.wmilia_m.decorators import *
+
+
+@app.on_message(
+    custom_filter.command(commands=["joke", "jokes", "funny"], disable=True)
+)
+@rate_limit(10, 60)
+async def joke(client, message):
+    try:
+        joke = get("https://v2.jokeapi.dev/joke/Any").json()
+        try:
+            get1 = joke["setup"]
+            get2 = joke["delivery"]
+            await message.reply(f"{get1}\n{get2}")
+        except BaseException:
+            get3 = joke["joke"]
+            await message.reply(get3)
+    except Exception as e:
+        return await message.reply(
+            str(e) + "\nPlease report to support chat @SpiralTechDivision"
+        )
