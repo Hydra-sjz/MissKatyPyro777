15d9db69ccdeef3c23ca348a3ed7a900849a2d0e
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-11-11 22:59:42 +0530

    Update admin.py

--- misskaty/plugins/admin.py
+++ misskaty/plugins/admin.py
@@ -89,6 +89,28 @@ __HELP__ = """
 /mentionall - Mention all members in a groups.
 """
 
+import html
+
+from misskaty import app as pgram
+
+
+@pgram.on_message(filters.command("pinned"))
+async def pinned(client, message):
+    chat_id = message.chat.id
+    chat_title = message.chat.title
+
+    chat_data = await pgram.get_chat(chat_id=chat_id)
+    if chat_data.pinned_message:
+        pinned_message_id = chat_data.pinned_message.id
+        message_link = (
+            f"http://t.me/c/{str(chat_id).replace(str(-100), '')}/{pinned_message_id}"
+        )
+        await message.reply(
+            f"The pinned message in {html.escape(chat_title)} is [here]({message_link})."
+        )
+
+    else:
+        await message.reply(f"There is no pinned message in {html.escape(chat_title)}.")
 
 # Admin cache reload
 @app.on_chat_member_updated(filters.group, group=5)
