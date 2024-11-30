f901db86242c1eb21cd4992bfd553f6a12dac0cc
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-11-30 22:03:11 +0530

    Update willam_bd.py

--- misskaty/ultis_ex/database/willam_bd.py
+++ misskaty/ultis_ex/database/willam_bd.py
@@ -1,5 +1,12 @@
 from async_pymongo import AsyncClient
-from misskaty.vars import DATABASE_URI
+from misskaty.vars import DATABASE_URI, DATABASE_NAME
 
 mongo_client = AsyncClient(DATABASE_URI)
 db = mongo_client.misskaty
+
+
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+
+
+mongo = MongoClient(DATABASE_URI)
+dbname = mongo[DATABASE_NAME]
