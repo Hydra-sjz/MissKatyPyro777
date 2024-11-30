988e62bf4cf7fe7e708b2f892f3d62289274fd89
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-11-30 21:54:05 +0530

    Update errors.py

--- misskaty/ultis_ex/decorators/errors.py
+++ misskaty/ultis_ex/decorators/errors.py
@@ -6,7 +6,7 @@ from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
 from misskaty.vars import LOG_GROUP_ID
-from misskaty import app
+from misskaty import app, DEV_USERS
 
 
 def split_limits(text):
@@ -51,3 +51,40 @@ def capture_err(func):
             raise err
 
     return capture
+
+
+# <============================================== IMPORTS =========================================================>
+from typing import Callable
+
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import Message
+
+#=====================================================================================================>
+
+
+# <================================================ FUNCTION =======================================================>
+def can_restrict(func: Callable) -> Callable:
+    async def non_admin(_, message: Message):
+        if message.from_user.id in DEV_USERS:
+            return await func(_, message)
+
+        check = await app.get_chat_member(message.chat.id, message.from_user.id)
+        if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await message.reply_text(
+                "» You're not an admin, Please stay in your limits."
+            )
+
+        admin = (
+            await app.get_chat_member(message.chat.id, message.from_user.id)
+        ).privileges
+        if admin.can_restrict_members:
+            return await func(_, message)
+        else:
+            return await message.reply_text(
+                "`You don't have permissions to restrict users in this chat."
+            )
+
+    return non_admin
+
+
+# <================================================ END =======================================================>
