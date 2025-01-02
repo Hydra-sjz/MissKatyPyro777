5803f0d7e2637bcb2a0518f5ab03cc1b9e20e336
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2025-01-02 23:56:02 +0530

    Update wall2.py

--- misskaty/plugins/wall2.py
+++ misskaty/plugins/wall2.py
@@ -5,7 +5,7 @@ from pyrogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from pyrogram import filters
 
 
-from misskaty import app as Mbot, CMD
+from misskaty import app as Mbot
 from walls import ANIM_PICS, ANIMALS_PICS, LOGO_PICS, CARS_PICS, DROWIG_PICS, FUNNY_PICS, ENTERT_PICS, GAME_PICS, LOVE_PICS, MUSIC_PICS, NATURE_PICS, SAYING_PICS, SPACE_PICS, COMIC_PICS, SPORT_PICS, PATTER_PICS, TECHNO_PICS, DESIN_PICS, HOLDAY_PICS, PEOPL_PICS, OTHERS_PICS                      
 from misskaty.vars import LOG_CHANNEL
 from misskaty.core import pyro_cooldown
